06c34877e53679664fb03c665af85a0e7bad2667
Christopher Karger <karger1986@gmail.com>
2020-10-30 22:33:20 +0100

    ref

--- send-email.ts
+++ send-email.ts
@@ -12,7 +12,7 @@ export const sendEmail = (text: string) => {
   const mailOptions = {
     from: process.env.Email,
     to: "karger1986@gmail.com",
-    subject: "Sending Email using Node.js",
+    subject: "Web Scraper News",
     text,
   };
   return transporter.sendMail(mailOptions, function (error, info) {
